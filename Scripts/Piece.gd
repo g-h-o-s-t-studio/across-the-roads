045d54afe724fce306ecfd791c614e9c11f1d87a
troubleShooter239 <morgun2282@gmail.com>
2024-02-07 19:35:42 +0300

    Fix bugs and add random speed for obstacles

--- Scripts/Piece.gd
+++ Scripts/Piece.gd
@@ -7,35 +7,36 @@ const OBSTACLES = [
 const SPAWN_DISTANCE = 20
 const OBSTACLE_SPEED = 5
 
-#func _ready():
-	#var right = Node.new()
-	#var left = Node.new()
-	#right.name = "RightToLeftObs"
-	#left.name = "LeftToRightObs"
-	#add_child(right)
-	#add_child(left)
+func _ready():
+	var right = Node.new()
+	var left = Node.new()
+	right.name = "RightToLeftObs"
+	left.name = "LeftToRightObs"
+	add_child(right)
+	add_child(left)
+
 
 func spawn_obstacles():
 	for obs in OBSTACLES:
 		obs = obs.instantiate()
-		obs.position.x = randf_range(SPAWN_DISTANCE, SPAWN_DISTANCE * 2)
+		obs.position.x = randf_range(position.x, position.x * 2)
 		if randi() % 2 == 0:
 			$RightToLeftObs.add_child(obs)
 		else:
 			$LeftToRightObs.add_child(obs)
 
 
-func determine_obstacle(obstacle):
+func determine_obstacle(obstacle, new_position):
+	obstacle.position.z += new_position
 	if abs(obstacle.position.z) > 20:
 			obstacle.queue_free()
 
 
 func _process(delta):
 	spawn_obstacles()
+	var pos = randf_range(OBSTACLE_SPEED, OBSTACLE_SPEED * 2.5) * delta
 	for obs in $RightToLeftObs.get_children():
-		obs.position.z += OBSTACLE_SPEED * delta
-		determine_obstacle(obs)
+		determine_obstacle(obs, pos)
 		
 	for obs in $LeftToRightObs.get_children():
-		obs.position.z -= OBSTACLE_SPEED * delta
-		determine_obstacle(obs)
+		determine_obstacle(obs, -pos)
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -6,7 +6,6 @@ const JUMP_VELOCITY = 4.5
 
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
-
 func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y -= gravity * delta
