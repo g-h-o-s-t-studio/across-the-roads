21878d86924380ab5d2a32e73f0e1dead1dcbb51
troubleShooter239 <morgun2282@gmail.com>
2024-02-09 13:42:11 +0300

    Fix level endless generation

--- Levels/GameController.gd
+++ Levels/GameController.gd
@@ -1,26 +1,29 @@
 extends Node
 
-const TILE_SCENE = preload("res://Levels/Shared/Tile.tscn")
-var tiles = []
+const MAP_SIZE: int = 5
+const TILE_SIZE: int = 50
+const TILE_SCENE: PackedScene = preload("res://Levels/Shared/Tile.tscn")
+var number_tiles: int = 1
+var tiles: Array[Object] = []
 
-#func _ready():
-	#for i in range(5):
-	#spawn_tile()
-#
-#func _process(delta):
-	#if $Player.position.x > tiles[0].position.x + 20:
-		#despawn_tile()
-		#spawn_tile()
-#
-#
-#func spawn_tile():
-	#var new_tile = TILE_SCENE.instantiate()
-	#new_tile.position = Vector3(tiles.size() * 20, 0, 0) # Предполагаемая длина сегмента карты
-	#add_child(new_tile)
-	#tiles.append(new_tile)
-	#
-#
-#
-#func despawn_tile():
-	#tiles[0].queue_free()
-	#tiles.remove(0)
+func _ready() -> void:
+	for i in range(MAP_SIZE):
+		spawn_tile()
+
+
+func _process(_delta: float) -> void:
+	if $Player.position.x > tiles[0].position.x + TILE_SIZE:
+		despawn_tile()
+		spawn_tile()
+
+
+func spawn_tile() -> void:
+	var new_tile: Object = TILE_SCENE.instantiate()
+	new_tile.position = Vector3(number_tiles * TILE_SIZE, 0, 0)
+	number_tiles += 1
+	add_child(new_tile)
+	tiles.append(new_tile)
+
+
+func despawn_tile() -> void:
+	tiles.pop_front().queue_free()
--- Scripts/Tile.gd
+++ Scripts/Tile.gd
@@ -1,42 +1,59 @@
 extends Node3D
 
-const OBSTACLES = [
+const OBSTACLES: Array[PackedScene] = [
 	preload("res://Levels/Obstacles/Square1.tscn"), 
-	preload("res://Levels/Obstacles/Rectangle1.tscn")
+	preload("res://Levels/Obstacles/Rectangle1.tscn"),
 ]
-const SPAWN_DISTANCE = 20
-const OBSTACLE_SPEED = 2
-# TODO: Fix spawn distance
-func _ready():
-	var right = Node.new()
-	var left = Node.new()
-	right.name = "RightToLeftObs"
-	left.name = "LeftToRightObs"
-	add_child(right)
-	add_child(left)
-	spawn_obstacles(randi_range(3, 10))
+const OBSTACLE_SPEED: int = 5
+const MIN_AMOUNT_OBS: int = 10
+const MAX_AMOUNT_OBS: int = 25
+const MAX_OBSTACLE_POSITION: int = 30
+@onready var right_to_left: Node = Node.new()
+@onready var left_to_right: Node = Node.new()
+@onready var raycast: RayCast3D = $RayCast3D
+#
+# TODO: Fix bug with raycast
+# можно получать начальную позицию тайла по иксу и конечную их их использовать
+# в функции рандома
+#
+func _ready() -> void:
+	add_child(right_to_left)
+	add_child(left_to_right)
+	spawn_obstacles(randi_range(MIN_AMOUNT_OBS, MAX_AMOUNT_OBS))
 
 
-func spawn_obstacles(number_obstacles):
-	for i in number_obstacles:
-		var obs = OBSTACLES[randi() % OBSTACLES.size() - 1].instantiate()
-		if randi() % 2 == 0:
-			obs.position = Vector3(randf_range(position.x, position.x * 2), 0, -10)
-			$RightToLeftObs.add_child(obs)
-		else:
-			obs.position = Vector3(randf_range(position.x, position.x * 2), 0, 10)
-			$LeftToRightObs.add_child(obs)
+func get_spawn_position(z: int) -> Vector3:
+	raycast.position = Vector3(randf_range(position.x, position.x * 2), 0, z)
+	while raycast.is_colliding():
+		raycast.position = Vector3(randf_range(position.x, position.x * 2), 0, z)
+	return raycast.position
 
 
-func move_obstacle(obstacle, new_position):
-	obstacle.position.z += new_position
-	if abs(obstacle.position.z) > 30:
-			obstacle.queue_free()
-
-
-func _process(delta):
-	for obs in $RightToLeftObs.get_children():
-		move_obstacle(obs, randf_range(OBSTACLE_SPEED, OBSTACLE_SPEED * 2.5) * delta)
+func spawn_obstacles(number_obstacles: int) -> void:
+	for i in range(number_obstacles):
+		var obs: Object = OBSTACLES[randi() % OBSTACLES.size() - 1].instantiate()
 		
-	for obs in $LeftToRightObs.get_children():
-		move_obstacle(obs, -randf_range(OBSTACLE_SPEED, OBSTACLE_SPEED * 2.5) * delta)
+		if randi() % 2 == 0:
+			obs.position = get_spawn_position(-10)
+			right_to_left.add_child(obs)
+		else:
+			obs.position = get_spawn_position(10)
+			left_to_right.add_child(obs)
+
+
+func move_obstacle(obstacle: StaticBody3D, new_position: float) -> void:
+	obstacle.position.z += new_position
+	if abs(obstacle.position.z) >= MAX_OBSTACLE_POSITION:
+		if obstacle.get_parent() == right_to_left:
+			right_to_left.remove_child(obstacle)
+			left_to_right.add_child(obstacle)
+		else:
+			left_to_right.remove_child(obstacle)
+			right_to_left.add_child(obstacle)
+
+func _process(delta: float) -> void:
+	for obs in right_to_left.get_children():
+		move_obstacle(obs, OBSTACLE_SPEED * delta)
+		
+	for obs in left_to_right.get_children():
+		move_obstacle(obs, -OBSTACLE_SPEED * delta)
